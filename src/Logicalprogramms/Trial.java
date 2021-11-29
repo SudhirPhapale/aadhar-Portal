ab0be147aa95acc5627b0dca99323ba994a07116
phapa <phapa@MSI>
2021-11-29 20:23:47 +0530

    [sudhir]:project addet to git

---
+++ src/Logicalprogramms/Trial.java
@@ -0,0 +1,31 @@
+package Logicalprogramms;
+
+public class Trial {
+
+	public static void main(String[] args) {
+		
+    String str= "sudhirir";
+    
+    int length = str.length();
+    char[] ch = str.toCharArray();
+    for(int i=0; i<length; i++)
+    {
+    	for(int j=i+1;j<length; j++ )
+    	{
+    		
+    		if (ch[i]== ch[j])
+    		{
+    	
+    	System.out.println(ch[j]);
+    	
+    	break;
+    		}
+    	
+    }
+    
+    
+	}
+    }     
+        		  
+	
+}
